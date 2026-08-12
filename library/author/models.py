8f3e4aa73b0e472bfabadd536af8055392dafa1e
marjanaL <liylukm@gmail.com>
2026-08-12 13:29:49 +0300

    Add primary key field 'id' to Author model

--- library/author/models.py
+++ library/author/models.py
@@ -15,6 +15,7 @@ class Author(models.Model):
     name = models.CharField(max_length=20)
     surname = models.CharField(max_length=20)
     patronymic = models.CharField(max_length=20)
+    id = models.AutoField(primary_key=True)
 
     def __str__(self):
         """
